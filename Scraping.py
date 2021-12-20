ce152e9eb2ff84fbbf2ef7795e255c756953f0ff
Merzaad <88736874+Merzaad@users.noreply.github.com>
2021-12-20 18:53:56 +0330

    find keywords

--- Scraping.py
+++ Scraping.py
@@ -1,16 +1,26 @@
 import requests
 import pandas as pd 
 from bs4 import BeautifulSoup 
+
 url = 'https://news.yahoo.com/'
 page = requests.get(url)
 soup = BeautifulSoup(page.content,'html.parser')
 results = soup.find(id="YDC-Stream")
 job_elements = results.find_all("div", class_="Cf")
+
 a= []
 for job_element in job_elements:
     title_element = job_element.find("h3")
     a.append(title_element.text.strip())
-print(a)
 
-#df = pd.DataFrame(a,columns=['Title'])
-#df.to_csv('yahoo_news.csv' , index = False)
+yahoo_news = pd.DataFrame(a,columns=['Titles'])
+yahoo_news.to_csv('yahoo_news.csv' , index = False)
+
+
+corona = results.find_all(
+    "h3", string=lambda text: "corona" in str(text).lower()
+)
+
+for i in corona:
+    i.find_all("h3")
+    print(i.text)
